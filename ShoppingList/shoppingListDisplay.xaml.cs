77c5532a3f1bff766cf03b06ec04f17c3dd1975e
Kendall Price <kprice147@users.campus>
2020-02-24 17:12:56 -0600

    In class work

---
+++ ShoppingList/shoppingListDisplay.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for shoppingListDisplay.xaml
+    /// </summary>
+    public partial class shoppingListDisplay : UserControl
+    {
+        public shoppingListDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDeleteItemButtonClicked(object sender,RoutedEventArgs e)
+        {
+            if(DataContext is ShoppingListData data)
+            {
+                if(sender is Button button)
+                {
+                    data.Remove(button.DataContext.ToString());
+                }
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
